a8a1348714a6660a3feb515a87bd94bf8c2e879b
Negar Moshtaghi <negar.moshtaghi@gmail.com>
2018-12-27 14:49:20 +0330

    fix settoken

--- Questioner/AppDelegate.swift
+++ Questioner/AppDelegate.swift
@@ -18,9 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
 
 
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        let defaults = UserDefaults.standard
 
-        let dataDict: [String: String] = ["token": fcmToken, "deviceId": UIDevice.current.identifierForVendor!.uuidString, "deviceName": UIDevice.current.name]
-        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        defaults.set(fcmToken, forKey: "Token")
+        defaults.set(UIDevice.current.identifierForVendor!.uuidString, forKey: "DeviceID")
+        defaults.set(UIDevice.current.name, forKey: "DeviceName")
 
         // TODO: If necessary send token to application server.
         // Note: This callback is fired at each app startup and whenever a new token is generated.
